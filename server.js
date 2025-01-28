62a20d64546f28452b97a8e7a4ff5ab7b928b51e
mubashira18 <liferacer1718@gmail.com>
2025-01-28 10:44:30 +0530

    firest commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "rvenkateswarreddy12345@gmail.com",
+    pass: "ixre adqd kylo ojiz",
+  },
+});
+
+app.post("/send-email", (req, res) => {
+  const { to, subject, text } = req.body;
+
+  const mailOptions = {
+    from: "rvenkateswarreddy12345@gmail.com",
+    to: to.join(", "), // Join email addresses into a single string
+    subject,
+    text,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send(error.toString());
+    }
+    res.status(200).send("Email sent: " + info.response);
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
